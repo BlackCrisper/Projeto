38492230b07a492a7ebd000802cf92a4264b6ed8
jureguelenda <davidfreitassouza081@gmail.com>
2022-12-14 09:27:10 -0300

    sql

--- Projeto-tcc/TCC-2.0/obj/Debug/net6.0/TCC-2.0.AssemblyInfo.cs
+++ Projeto-tcc/TCC-2.0/obj/Debug/net6.0/TCC-2.0.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-TCC_2._0-C4AE0724-531A-46F1-B380-5E95C1040765")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCC-2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
